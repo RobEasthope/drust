bcf3b056b6de52c409eb594cf375aef31da4d50d
Rob Easthope <rob@projectinnerspace.org>
2024-10-04 16:22:25 +0100

    Fix TSC issue

--- app/utils/useRootLoaderData.ts
+++ app/utils/useRootLoaderData.ts
@@ -1,10 +1,10 @@
 import { useRouteLoaderData } from "@remix-run/react";
 
-import type { Loader as RootLoader } from "~/root";
+import type { loader as RootLoader } from "~/root";
 
 export function useRootLoaderData() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const data = useRouteLoaderData<RootLoader>(`root`);
+  const data = useRouteLoaderData<typeof RootLoader>(`root`);
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return data;
